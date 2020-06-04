ce9596b00b2e13d783e7a3aeb607f6cf3ecc5ac3
sirwhod <rodrigo.brandao98@gmail.com>
2020-06-04 00:18:24 -0300

    Mudança CSS Modal

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -5,7 +5,7 @@ function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() )
+    .then( res => res.json() ) //function(res) {res.json()}
     .then( states => {
 
         for( const state of states ) {
